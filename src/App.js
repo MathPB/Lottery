da1a06e03d3b9a0c13feac05337627649638fa6b
unknown <matheus.pereira4025@hotmail.com>
2019-10-04 02:15:48 -0300

    Update Lottery contract 02:15 04/10/2019

--- src/App.js
+++ src/App.js
@@ -1,29 +1,51 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
 import web3 from './web3.js';
- 
+import lottery from './lottery';
+
 class App extends Component {
+  state = {
+    manager: '',
+    players: [],
+    balance: '',
+    value: ''
+  };
+
+  async componentDidMount(){
+    const manager = await lottery.methods.manager().call();
+    const players = await lottery.methods.getPlayers().call();
+    const balance = await web3.eth.getBalance(lottery.options.address);
+
+    this.setState({ manager, players, balance });
+  }
+
+
   render(){
-    window.ethereum.enable()
-      .then(web3.eth.getAccounts()
-        .then(console.log));
+    // window.ethereum.enable()
+    //   .then(web3.eth.getAccounts()
+    //     .then(console.log));
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Lottery Contract</h2>
+      <p>
+        This contract is managed by {this.state.manager}.<br></br>
+        There are currently {this.state.players.length} people entered, 
+        competing to win {web3.utils.fromWei(this.state.balance, 'ether')} ether!
+      </p>
+      <hr/>
+
+      <form onSubmit={this.onSubmit}>
+        <h4>Want to try you luck?</h4>
+        <div>
+          <label>Amout of ether to enter:</label>
+          <br/>
+          <input
+            value = {this.state.value}
+            onChange={event => this.setState({ value: event.target.value })}
+          />
+        </div>
+        <button>Enter</button>
+      </form>
     </div>
   );
 }}
